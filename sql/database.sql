074ac15815095d4589a365c51b03f67ef5c012bb
Hendro Wibowo <hendrothemail@gmail.com>
2022-01-18 15:25:25 +0700

    Minor changes

--- sql/database.sql
+++ sql/database.sql
@@ -5,9 +5,6 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-CREATE DATABASE `finland_places` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `finland_places`;
-
 SET NAMES utf8mb4;
 
 DROP TABLE IF EXISTS `categories`;
